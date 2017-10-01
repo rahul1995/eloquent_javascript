ecb4c972e9565c3a3a943982aa123c7075fea84a
Rahul Jain <jainrahul6620@gmail.com>
2017-10-01 11:08:56 +0530

    Removing escape character inside square brackets/choose one[]

--- 09_regexp/03_numbers_again.js
+++ 09_regexp/03_numbers_again.js
@@ -1,5 +1,5 @@
 // Fill in this regular expression.
-var number = /^(\+|-)?(\d+\.?\d*|\d*\.?\d+)(e[\+-]?\d+)?$/i;
+var number = /^(\+|-)?(\d+\.?\d*|\d*\.?\d+)(e[+-]?\d+)?$/i;
 
 // Tests:
 ["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
