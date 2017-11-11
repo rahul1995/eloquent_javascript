b0732458886f28f80ee5e4009a70d120037bc279
Rahul Jain <jainrahul6620@gmail.com>
2017-11-11 11:19:47 +0530

    Use ES6 syntax

--- 13_dom/01_build_a_table.js
+++ 13_dom/01_build_a_table.js
@@ -1,33 +1,36 @@
 function buildHeader(headers) {
-    var row = document.createElement("tr");
-    headers.forEach(function(el) {
-      var col = document.createElement("th");
+    const row = document.createElement("tr");
+    headers.forEach(el => {
+      const col = document.createElement("th");
       col.textContent = el.name;
-      if(el.type === "number") col.style.textAlign = "right";
+      if(el.type === "number") {
+        col.style.textAlign = "right";
+      }
       row.appendChild(col);
     });
     return row;
   }
   function buildRow(ob, props) {
-    var row = document.createElement("tr");
-    props.forEach(function(prop) {
-      var col = document.createElement("td");
+    const row = document.createElement("tr");
+    props.forEach(prop => {
+      const col = document.createElement("td");
       col.textContent = ob[prop.name];
-      if(prop.type === "number") col.style.textAlign = "right";
+      if(prop.type === "number") {
+        col.style.textAlign = "right";
+      }
       row.appendChild(col);
     });
     return row;
   }
   function buildTable(data) {
     // Your code here.
-    var colNames = Object.keys(data[0]);
-    var table = document.createElement("table");
-    colNames = colNames.map(function(el) {
+    const colNames = Object.keys(data[0]).map(function(el) {
       return {
         "name": el,
         "type": typeof data[0][el]
       };
     });
+    const table = document.createElement("table");
     table.appendChild(buildHeader(colNames));
     data.forEach(function(el) {
       table.appendChild(buildRow(el, colNames));
